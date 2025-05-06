aad9fd6457d0ec7bc071b0adaa6bfffb27888506
Damian5314 <damianwillmse@gmail.com>
2025-05-06 20:35:28 +0200

    Made footer

--- mijn-portfolio/src/App.jsx
+++ mijn-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       </nav>
       <hr />
       <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ mijn-portfolio/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="footer-left">
+          <h2>Let's start<br />creating together</h2>
+          <a href="mailto:damianwillmse@gmail.com" className="footer-button">
+            Let’s talk ↗
+          </a>
+          <p className="footer-copyright">© Damian {new Date().getFullYear()}</p>
+        </div>
+  
+        <div className="footer-right">
+          <div className="footer-links">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/projects">Werk</a></li>
+              <li><a href="/about">Over mij</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+  
+            <ul>
+              <li><a href="https://www.linkedin.com/in/damianwillemse/" target="_blank" rel="noreferrer">Linkedin ↗</a></li>
+              <li><a href="https://github.com/Damian5314" target="_blank" rel="noreferrer">Github ↗</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
