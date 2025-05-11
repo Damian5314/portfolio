22ba2fcc1612f651f429b95cb08d918c664b6218
Damian5314 <damianwillmse@gmail.com>
2025-05-11 19:48:56 +0200

    Added more on the about me page with styling

--- mijn-portfolio/src/pages/About.jsx
+++ mijn-portfolio/src/pages/About.jsx
@@ -1,11 +1,93 @@
+import './About.css';
+
 function About() {
-    return (
-      <div className="container">
-        <h1>Over mij!</h1>
-        <p>Verhaaltje.</p>
+  return (
+    <div className="container">
+      <div className="row">
+        {/* Vaardigheden */}
+        <div className="col-lg-4 d-flex flex-column vaardigheden-container">
+          <div className="tag">
+            <p>Skills</p>
+          </div>
+          <h2 className="title">Vaardigheden</h2>
+
+          <div className="werkgebied-container">
+            <h5 className="titel-werkgebied">Werkgebied</h5>
+            {[
+              "HTML",
+              "CSS",
+              "JavaScript",
+              "Python",
+              "TypeScript",
+              "React",
+              "C#"
+            ].map((skill, i) => (
+              <div className="d-flex skill-item" key={i}>
+                <i className="fa-solid fa-circle-check"></i>
+                <p>{skill}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className="talen-container">
+            <h5 className="titel-talen">Talen</h5>
+            {[
+              "Nederlands (moedertaal)",
+              "Engels (professionele vaardigheid)",
+            ].map((language, i) => (
+              <div className="d-flex taal-item" key={i}>
+                <div className="country-flag-image-container"></div>
+                <p>{language}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Werkervaring */}
+        <div className="col-lg-4 d-flex flex-column werkervaring-container">
+          <div className="tag">
+            <p>Functies</p>
+          </div>
+          <h2 className="title">Werkervaring</h2>
+
+          <div className="jobs-container">
+            {[
+              ["21South", "Service Desk", "Okt. 2024 - heden"],
+              ["Mediamarkt", "Logistiek medewerker", "jul. 2021 - okt. 2024"],
+              ["Dirk", "Vakkenvuller", "jul. 2019 - jul. 2021"],
+            ].map(([company, role, period], i) => (
+              <div className="d-flex flex-column job-item" key={i}>
+                <h6>{company}</h6>
+                <p className="job-function">{role}</p>
+                <p className="job-period">{period}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Opleiding */}
+        <div className="col-lg-4 d-flex flex-column opleiding-container">
+          <div className="tag">
+            <p>Educatie</p>
+          </div>
+          <h2 className="title">Opleiding</h2>
+
+          <div className="scholen-container">
+            {[
+              ["Hogeschool Rotterdam", "Informatica (HBO)", "2023 - heden"],
+              ["Montfort College Rotterdam", "HBO", "2016 - 2023"],
+            ].map(([school, edu, year], i) => (
+              <div className="d-flex flex-column school-item" key={i}>
+                <h6>{school}</h6>
+                <p className="school-edu-name">{edu}</p>
+                <p className="school-year">{year}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default About;
-  
+    </div>
+  );
+}
+
+export default About;
