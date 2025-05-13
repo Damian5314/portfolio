f0533e603db68b6b3da06443fc117c191ad00b15
Damian5314 <damianwillmse@gmail.com>
2025-05-13 18:28:56 +0200

    Added a picture of me on homepage with styling

--- mijn-portfolio/src/pages/Home.jsx
+++ mijn-portfolio/src/pages/Home.jsx
@@ -1,11 +1,21 @@
+import './Home.css';
+
 function Home() {
-    return (
-      <div className="container">
+  return (
+    <div className="home-container">
+      <img
+        src="DamianWillemse.jpeg"
+        alt="Damian Willemse"
+        className="profile-image"
+      />
+      <div className="intro-text">
         <h1>Welkom!</h1>
-        <p>Dit is mijn persoonlijke portfolio.</p>
+        <p>
+          Dit is mijn persoonlijke portfolio. Hier deel ik mijn werk, skills en projecten waar ik trots op ben.
+        </p>
       </div>
-    );
-  }
-  
-  export default Home;
-  
+    </div>
+  );
+}
+
+export default Home;
