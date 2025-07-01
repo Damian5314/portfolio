667158c41195a1548684a09b869f6946d66cf447
Damian5314 <damianwillmse@gmail.com>
2025-07-01 16:17:34 +0200

    lala

--- mijn-portfolio/src/pages/Home.jsx
+++ mijn-portfolio/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css';
 import { useLanguage } from '../context/LanguageContext';
 import translations from '../data/translations';
+import { Link } from 'react-router-dom';
 
 function Home() {
   const { language } = useLanguage();
@@ -16,8 +17,8 @@ function Home() {
         <h1>{t.welcome}</h1>
         <p>{t.intro}</p>
         <div className="home-buttons">
-          <a href="projects" className="home-button">{t.viewProjects}</a>
-          <a href="contact" className="home-button secondary">{t.contactMe}</a>
+          <Link to="/projects" className="home-button">{t.viewProjects}</Link>
+          <Link to="/contact" className="home-button secondary">{t.contactMe}</Link>
         </div>
       </div>
     </div>
